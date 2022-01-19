17d586b0bed38a3f1affda7f2eb9ec7d26f53efe
Rafsun Masud <rafsun82@gmail.com>
2022-01-18 20:35:54 -0500

    Initial commit

---
+++ src/main/java/com/r5n/gradetrackerapi/GradeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.r5n.gradetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r5n/gradetrackerapi/GradeTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r5n.gradetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
