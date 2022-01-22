95d8ae851f1912023e12e420b4a093d0631ea552
Rafsun Masud <rafsun82@gmail.com>
2022-01-22 16:47:08 -0500

    Add activity entity

---
+++ src/main/java/com/r5n/gradetrackerapi/model/Activity.java
@@ -0,0 +1,48 @@
+package com.r5n.gradetrackerapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "activities")
+public class Activity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "total")
+    private Double total;
+
+    @Column(name = "score")
+    private Double score;
+
+    @Column(name = "pos")
+    @JsonIgnore
+    private Integer order;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id", nullable = false)
+    @JsonIgnore
+    private Class belongingClass;
+
+    public Activity() {
+    }
+
+    public Activity(String name, Double weight, Double total) {
+        this.name = name;
+        this.weight = weight;
+        this.total = total;
+    }
+}
